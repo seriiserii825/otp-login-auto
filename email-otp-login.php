296a54f485094e916a1f30d23328385d04ffd8b9
seriiserii825 <seriiburduja@gmail.com>
2026-02-23 18:43:24 +0200

    feat: admin settings url

--- email-otp-login.php
+++ email-otp-login.php
@@ -1,15 +1,21 @@
 <?php
 
 /**
- * Plugin Name: Email OTP Login Custom (Passwordless)
+ * Plugin Name: OTP Login Auto
  * Description: Passwordless login via email + OTP with domain restriction
- * Name: email-otp-login-custom
  * Version: 1.0
+ * Author: serii
  */
 
 if (!defined('ABSPATH')) exit;
 
 define('EOL_PLUGIN_FILE', __FILE__);
+
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
+  $settings_link = '<a href="' . admin_url('options-general.php?page=eol-settings') . '">Settings</a>';
+  array_unshift($links, $settings_link);
+  return $links;
+});
 
 require_once __DIR__ . '/includes/config.php';
 require_once __DIR__ . '/includes/settings.php';
